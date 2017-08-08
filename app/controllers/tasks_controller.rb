141ec9c2b67a7266dceb346cbfe66a72dd77aec3
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-08 12:44:12 +0200

    Implemented create and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,40 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.new(task_params)
+    @task.update(params[:task])
+
+    redirect_to task_path(@task)
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :done)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+40.times do
+  Task.create!(
+    name: Faker::Zelda.character,
+    description: Faker::Zelda.location,
+  )
+end
